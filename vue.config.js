f9caf8638ffd09667f26423853a4f4e2f8318bea
MirrorXu <xuxulee@163.com>
2024-08-04 17:06:11 +0800

    引入本地vue进行源码分析，通过配置 webpack的alias实现

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require("@vue/cli-service");
+const path = require("node:path");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      // extension:[], //内部已默认配置
+      alias: {
+        // vue: path.resolve(
+        //   __dirname,
+        //   "vue/src/platforms/web/entry-runtime-with-compiler-esm.ts"
+        // ),
+        vue$: path.resolve(__dirname, "./vue/dist/vue.runtime.esm.js"),
+      },
+    },
+  },
 });
